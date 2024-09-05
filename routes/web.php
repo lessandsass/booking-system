f7e4efdfffca40f1781341d0755fd75d42d21484
Less <31947204+lessandsass@users.noreply.github.com>
2024-09-05 18:19:04 +0530

    7.working-with-date-periods
    - The Spatie Period package was pulled over using the composer command
    - The welcome route was commented over.
    - The ScheduleAvailability class was created inside the \app\Bookings directory and the namespace was added.
    - The $availability variable was created inside the welcome route.
    - The PeriodCollection $periods variable had been calledout inside the ScheduleAvailability class.
    - The __construct method had been created for that PeriodCollection class. The custom period collection was created
    - The employee availability was added.
    - forPeried function had been created
    - The specific period was added to the PeriodCollection in the ScheduleAvailability class

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Bookings\ScheduleAvailability;
 use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    return view('welcome');
+    $availability = (new ScheduleAvailability())
+        ->forPeriod();
+
+    // return view('welcome');
 });
+
+
