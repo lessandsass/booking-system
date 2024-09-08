a29b948d39203f48678f0f778bd14e9acfe54f9c
Less <31947204+lessandsass@users.noreply.github.com>
2024-09-08 16:05:11 +0530

    no errors it is how the algorithm work
    - No error was found, here the given peried start from now, it also checking the
        currently date, so the schedule date have to be the current date. if the code not work change or update the schedule to current date.
    - here the code also check the availability from the today's or current date

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@ Route::get('/', function () {
     $employee = Employee::find(1);
     $service = Service::find(1);
 
+    /*
+    # No error was found, here the given peried start from now, it also checking the
+    currently date, so the schedule date have to be the current date. if the code not work change or update the schedule to current date.
+    # here the code also check the availability from the today's or current date
+    */
     $availability = (new ScheduleAvailability($employee, $service))
         ->forPeriod(
             now()->startOfDay(),
