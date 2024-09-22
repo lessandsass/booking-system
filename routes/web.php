65f8fac73693cbe64ce9dac871da64887dd7b23d
Less <31947204+lessandsass@users.noreply.github.com>
2024-09-22 08:42:22 +0530

    14.Building the service availability generator
    - All the scripts inside the welcome route was commendted over
    - So far we have built, the period the employee can work
    - A new class was created ServiceSlotAvailability
    - We are going to accept the colleciton of employees and service here
    
    - We need the slot range generator to create empty slots, the line of code was copied from the web.php file and pasted over to the ServiceSlotAvailability’s forPeriod method and the time was added dynamically as $startAt and $endAt.
    
    - The duration for hair service was changed to 45 minute
    - The result: the slot will be reduced.
    - check the employee availability add the them into slots.
    - We are goint implement this 4 things in the ServiceSlotAvailability’s forPeriod method
        - get the availability for the employee
        - remove the appointment form the period collection
        - add the available employees to the $range
        - remove empty slots

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,28 @@ use App\Models\Service;
 use App\Models\Employee;
 use Illuminate\Support\Facades\Route;
 use App\Bookings\ScheduleAvailability;
+use App\Bookings\ServiceSlotAvailability;
 use App\Bookings\SlotRangeGenerator;
 
 // Carbon::setTestNow(now()->setTimeFromTimeString('12:00'));
 
 Route::get('/', function () {
 
-    $generator = (new SlotRangeGenerator(
-                        now()->startOfDay(),
-                        now()->addDay()->endOfDay())
-                );
+    $employees = Employee::get();
+    $service = Service::find(1);
 
-    dd($generator->generate(30));
+    $availability = (new ServiceSlotAvailability($employees, $service))
+        ->forPeriod(
+            now()->startOfDay(),
+            now()->addDay()->endOfDay()
+        );
+
+    // $generator = (new SlotRangeGenerator(
+    //                 now()->startOfDay(),
+    //                 now()->addDay()->endOfDay())
+    //             );
+
+    // dd($generator->generate(30));
 
     // $employee = Employee::find(1);
     // $service = Service::find(1);
