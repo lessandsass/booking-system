3dbbb825cc6a5e53bd3f3577b37d26263f880a09
Less <31947204+lessandsass@users.noreply.github.com>
2024-09-28 07:46:20 +0530

    lists multiple slots over more than one day-test
    - the period was extended up to end of the year
    - A extra day was added on the $availability
    - we are mapping over the DateCollection inside the closure

--- tests/Feature/Bookings/ServiceSlotAvailabilityTest.php
+++ tests/Feature/Bookings/ServiceSlotAvailabilityTest.php
@@ -1,7 +1,6 @@
 <?php
 
 /*
-    2. lists multiple slots over more than one day
     3. excludes booked appointments for the employee
     4. ignores cancelled appointments
     5. shows multiple employees available for service
@@ -31,6 +30,33 @@ it('shows available time slots for a service', function () {
                     ->forPeriod(now()->startOfDay(), now()->endOfDay());
 
     expect($availability->first()->date->toDateString())->toEqual(now()->toDateString());
+
+    expect($availability->first()->slots)->toHaveCount(16);
+
+});
+
+it('lists multiple slots over more than one day', function () {
+    Carbon::setTestNow(Carbon::parse('1st January 2000'));
+
+    $employee = Employee::factory()
+                    ->has(Schedule::factory()->state([
+                        'starts_at' => now()->startOfDay(),
+                        'ends_at' => now()->endOfYear(),
+                    ]))
+                    ->create();
+
+    $service = Service::factory()->create([
+        'duration' => 30
+    ]);
+
+    $availability = (new ServiceSlotAvailability(collect([$employee]), $service))
+                    ->forPeriod(now()->startOfDay(), now()->addDay()->endOfDay());
+
+    expect($availability->map(fn ($date) => $date->date->toDateString()))
+                    ->toContain(
+                        now()->toDateString(),
+                        now()->addDay()->toDateString()
+                    );
 
     expect($availability->first()->slots)->toHaveCount(16);
 
