ccddd8999eb0d3ef6715cb58039920b7d3a71ae1
Less <31947204+lessandsass@users.noreply.github.com>
2024-10-01 15:00:23 +0530

    24. Installing-alpinejs
    - install the alpinejs
    - callout the alpinejs in the resources/js/app.js file
    - The vite helper was add in the views\components\app-layout.blade.php
    - Restart the  server
    - Check the alpinejs. bookings\checkout.blade.php

--- resources/views/bookings/checkout.blade.php
+++ resources/views/bookings/checkout.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <div class="space-y-12">
+
         <div>
             <h2 class="text-xl font-medium mt-3">Here's what you're booking</h2>
             <div class="flex mt-6 space-x-3 bg-slate-100 rounded-lg p-4">
@@ -22,10 +23,19 @@
 
         <div>
             <h2 class="text-xl font-medium mt-3">1. When for?</h2>
-            <div>
+            <div
+                x-data="{
+                    picker: null
+                }"
+                x-init="
+                    picker='abcdf'
+                    console.log(picker)
+                "
+            >
                 Calender
             </div>
         </div>
+
     </div>
 </x-app-layout>
 
--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -7,6 +7,7 @@
     <title>Bookings</title>
 
     @vite('resources/css/app.css')
+    @vite(['resources/js/app.js'])
 
 </head>
 <body>
